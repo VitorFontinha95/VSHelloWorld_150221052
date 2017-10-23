b6633146772b1c64fede79da21966762ad918634
Vitor Fontinha <vitor.almeida.fontinha@gmail.com>
2017-10-23 14:18:39 +0100

    Efetuado um comentário no ficheiro Program.cs

---
+++ VSHelloWorld_150221052/VSHelloWorld_150221052/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace VSHelloWorld_150221052
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Comentário de Teste
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
